cc790b38abcc95f72ca90817cd03f8570baddb8a
Mohamed Abdallah <mo7amed.3bdalla7@gmail.com>
2019-06-10 01:50:09 +0200

    forget password fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api')->except(['register', 'login', 'refresh']);
+        $this->middleware('auth:api')->except(['register', 'login', 'refresh','forget']);
     }
 
 
